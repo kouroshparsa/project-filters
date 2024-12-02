db6119d6eab83c41cf1bfc1b26963cf01b76d75e
Kourosh Parsa <kouroshtheking@gmail.com>
2024-12-02 15:00:27 -0800

    Update sp1.py

--- app2/sp1.py
+++ app2/sp1.py
@@ -3,3 +3,6 @@ from hl7_tea import Message
 msg = Message(os.getenv('msg'))
 res = msg
 res.set_field('GT1-1', '123')
+print(res.get_field('GT1-1'))
+control_id = res.get_field('MSH-10')
+print(f'control id={control_id}')
